c2408e051e9fe9911146908f16637925cea15ac7
Nick M <nmmauro24@gmail.com>
2025-03-23 17:18:33 -0400

    Angular auth interceptor and routes config

--- library-management-ui/src/app/app.routes.ts
+++ library-management-ui/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { BookListComponent } from './components/books/book-list/book-list.component';
+import { BookDetailsComponent } from './components/books/book-details/book-details.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/books', pathMatch: 'full' },
+  { path: 'books', component: BookListComponent },
+  { path: 'books/:id', component: BookDetailsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: '/books' }
+];
