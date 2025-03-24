fa93eab40892b8be11260cde4b32264857fbc2f7
Nick M <nmmauro24@gmail.com>
2025-03-23 20:58:49 -0400

    Replaced db connection string to generic for replacement

--- library-management-ui/src/app/core/environment.ts
+++ library-management-ui/src/app/core/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'https://localhost:7051/api'
-  };
+  production: false,
+  apiUrl: 'https://localhost:7051/api' // Update to match your actual API port
+};
